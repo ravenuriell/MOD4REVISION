163807007c4c97f82a64648a0947ffc2b89d0c07
Raven Uriel <91958584+ravenuriell@users.noreply.github.com>
2024-07-17 22:09:22 +0800

    Add files via upload

---
+++ mod4.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenGamingToken is ERC20, Ownable {
+    string private constant _tokenName = "Degen";
+    string private constant _tokenSymbol = "DGN";
+
+    struct Item {
+        string name;
+        uint256 price;
+    }
+
+    Item[] public items;
+
+    mapping(address => Item[]) public playerItems;
+
+    constructor(address initialOwner) ERC20(_tokenName, _tokenSymbol) Ownable(initialOwner) {
+        items.push(Item("Concealer", 100 * 10 ** decimals()));
+        items.push(Item("Foundation", 150 * 10 ** decimals()));
+        items.push(Item("Lipstick", 50 * 10 ** decimals()));
+        items.push(Item("Eyeshadow Pallete", 200 * 10 ** decimals()));
+        items.push(Item("Mascara", 70 * 10 ** decimals()));
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function redeem(uint256 itemId, address to) public {
+        require(itemId < items.length, "Invalid item ID");
+        Item memory item = items[itemId];
+        _burn(msg.sender, item.price);
+        playerItems[to].push(item); 
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function getItemsCount() public view returns (uint256) {
+        return items.length;
+    }
+
+    function getItem(uint256 itemId) public view returns (string memory itemName, uint256 price) {
+        require(itemId < items.length, "Invalid item ID");
+        Item storage item = items[itemId];
+        return (item.name, item.price);
+    }
+
+    function getPlayerItems(address player) public view returns (Item[] memory) {
+        return playerItems[player];
+    }
+
+    function confirmItemDelivery(address player, uint256 itemId) public view returns (string memory itemName, uint256 price, bool delivered) {
+        require(itemId < playerItems[player].length, "Invalid item ID");
+        Item memory item = playerItems[player][itemId];
+        return (item.name, item.price, true);
+    }
+}
